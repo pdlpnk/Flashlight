022cdbaf0b06942c564ba6e91285d349d016d4ad
Juicefai <rimchik3010@gmail.com>
2024-10-07 17:57:39 +0300

    Initial Commit

---
+++ FlashlightTests/FlashlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashlightTests.swift
+//  FlashlightTests
+//
+//  Created by Рома Подлипняк on 07.10.2024.
+//
+
+import Testing
+@testable import Flashlight
+
+struct FlashlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
